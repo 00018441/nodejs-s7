e0761a32e25bea100f43b5280de8d1c021389439
Anvar Madvaliyev <00018441@wiut.uz>
2025-02-25 09:29:56 +0500

    batman v1

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const PORT = 6969;
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello world</h1>");
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on PORT ${PORT}`);
+});
